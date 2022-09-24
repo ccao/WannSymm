dbf38c2d8cd4c00407af7c60908947e442843afb
gxzhi <guoxiang.zhi@gmail.com>
2022-09-24 11:26:36 +0800

    v1.2.0 local-axis done

--- src/version.h
+++ src/version.h
@@ -3,11 +3,11 @@
 #define VERSION_H
 
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 1
-#define VERSION_PATCH 2
+#define VERSION_MINOR 2
+#define VERSION_PATCH 0
 
 //#define PRE_RELEASE_ALPHA
-#define PRE_RELEASE_BETA
+//#define PRE_RELEASE_BETA
 //#define PRE_RELEASE_CANDIDATE
 
 //#define VERSION_PRE 11202
